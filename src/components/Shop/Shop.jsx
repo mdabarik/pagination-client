abe1fef91acb62eaa3a063f0203bcbd20ad9f4a5
mdabarik <mdabarik19@gmail.com>
2023-11-01 12:40:15 +0600

    first commit

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,17 +3,35 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
 
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(0);
+    const { count } = useLoaderData();
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+
+    // const pages = []
+    // for (let i = 0; i < numberOfPages; i++) {
+    //     pages.push(i);
+    // }
+    const pages = [...Array(numberOfPages).keys()];
+
+    /*
+    Done 1: get the total number of products
+    Done 2: number of items per page dynamic
+    Todo 3: get the current page
+
+    */
+
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5555/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage, itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -62,25 +80,50 @@ const Shop = () => {
     }
 
     return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product => <Product
-                        key={product._id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
+
+        <div>
+            <div className='shop-container'>
+                <div className="products-container grid grid-cols-1">
+                    {
+                        products.map(product => <Product
+                            key={product._id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart
+                        cart={cart}
+                        handleClearCart={handleClearCart}
+                    >
+                        <Link className='proceed-link' to="/orders">
+                            <button className='btn-proceed'>Review Order</button>
+                        </Link>
+                    </Cart>
+                </div>
             </div>
-            <div className="cart-container">
-                <Cart
-                    cart={cart}
-                    handleClearCart={handleClearCart}
-                >
-                    <Link className='proceed-link' to="/orders">
-                        <button className='btn-proceed'>Review Order</button>
-                    </Link>
-                </Cart>
+            <div className='pagination'>
+                <p>Curr page: {currentPage}</p>
+                <button onClick={() => setCurrentPage(currentPage != 0 ? currentPage - 1 : 0)}>Prev</button>
+                {
+                    pages.map(page => <button
+                        className={currentPage === page ? 'selected' : ''}
+                        onClick={() => setCurrentPage(page)}
+                        key={page}>{page}</button>)
+                }
+                <button onClick={() => setCurrentPage(currentPage < pages.length - 1 ? currentPage + 1 : currentPage)}>Next</button>
+                <select value={itemsPerPage} onChange={(e) => {
+                    setItemsPerPage(e.target.value)
+                    setCurrentPage(0)
+                }}>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="30">30</option>
+                    <option value="40">40</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Shop></Shop>
+        element: <Shop></Shop>,
+        loader: () => fetch('http://localhost:5555/productsCount')
       },
       {
         path: 'orders',
